3e549c426d6242b4dbb8f1f95b2f2cf6e13d7cb1
shiwam ray <shiwamray623@gmail.com>
2022-12-26 20:43:43 +0530

    start project

---
+++ components/Carasol.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
+
+
+const Carasol = () => {
+  return (
+    <div className='caro-main-div'>
+          <Carousel>
+          <div className='carosel-div' style={{display:'flex',flexDirection:'row',flex:100}}>
+            <div style={{display:'flex',flex:50, height:'auto'}}>
+               <img src="image/img 1.jpg" alt=""/>
+            </div>
+            <div style={{display:'flex',flex:50}}>
+              <div className='king-div'>
+                <div>
+                <div className='prog-text'>
+                    <h3>Oota</h3>
+                    <h1>Empowering home chefs with virturl restaurants</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut nobis architecto nisi ullam, atque ipsa necessitatibus pariatur deserunt minima nam numquam, quidem tempore quod praesentium veniam non dolores dolorem in provident? Eaque ipsum veritatis, reiciendis laudantium unde quia. Pariatur, officiis.</p>
+                </div>
+                <div className='smart-btn'>
+                  <button>VIEW PORTFOLIO</button>
+                  <button>CONTACT US</button>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='carosel-div' style={{display:'flex',flexDirection:'row',flex:100}}>
+            <div style={{display:'flex',flex:50, height:'auto'}}>
+               <img src="image/img 1.jpg" alt=""/>
+            </div>
+            <div style={{display:'flex',flex:50}}>
+              <div className='king-div'>
+                <div>
+                <div className='prog-text'>
+                    <h3>Oota</h3>
+                    <h1>Empowering home chefs with virturl restaurants</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut nobis architecto nisi ullam, atque ipsa necessitatibus pariatur deserunt minima nam numquam, quidem tempore quod praesentium veniam non dolores dolorem in provident? Eaque ipsum veritatis, reiciendis laudantium unde quia. Pariatur, officiis.</p>
+                </div>
+                <div className='smart-btn'>
+                  <button>VIEW PORTFOLIO</button>
+                  <button>CONTACT US</button>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='carosel-div' style={{display:'flex',flexDirection:'row',flex:100}}>
+            <div style={{display:'flex',flex:50, height:'auto'}}>
+               <img src="image/img 1.jpg" alt=""/>
+            </div>
+            <div style={{display:'flex',flex:50}}>
+              <div className='king-div'>
+                <div>
+                <div className='prog-text'>
+                    <h3>Oota</h3>
+                    <h1>Empowering home chefs with virturl restaurants</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut nobis architecto nisi ullam, atque ipsa necessitatibus pariatur deserunt minima nam numquam, quidem tempore quod praesentium veniam non dolores dolorem in provident? Eaque ipsum veritatis, reiciendis laudantium unde quia. Pariatur, officiis.</p>
+                </div>
+                <div className='smart-btn'>
+                  <button>VIEW PORTFOLIO</button>
+                  <button>CONTACT US</button>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+                {/* <div className='carosel-div'>
+                    <div className='hard-img' style={{width : '600px' , position : 'relative'}}>
+                    <img src="image/img 1.jpg" />
+                    </div>
+                    <div className='allText-div'>
+                      <div >
+                        <h2>Oota</h2>
+                        <h1>Setting up Fix coding guide</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad fugit praesentium eveniet quasi soluta deleniti!</p>
+                        <button>VIEW PORTFOLIO</button>
+                        <button>CONTACT US</button>
+                      </div>
+                    </div>
+                </div>
+                <div className='carosel-div'>
+                    <div className='hard-img' style={{width : '600px' , height : '480px' , position : 'relative'}}>
+                    <img src="image/img 1.jpg" />
+                    </div>
+                    <div className='allText-div'>
+                      <div className='box-div'>
+                      <h1>Setting up Fix coding guide</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad fugit praesentium eveniet quasi soluta deleniti!</p>
+                      <button>GET IT TOUCH</button>
+                      </div>
+                    </div>
+                </div>
+                <div className='carosel-div'>
+                    <div className='hard-img' style={{width : '600px' , height : '480px' , position : 'relative'}}>
+                    <img src="image/img 1.jpg" />
+                    </div>
+                    <div className='allText-div'>
+                      <div className='box-div'>
+                      <h1>Setting up Fix coding guide</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad fugit praesentium eveniet quasi soluta deleniti!</p>
+                      <button>GET IT TOUCH</button>
+                      </div>
+                    </div>
+                </div> */}
+            </Carousel>
+    </div>
+  )
+}
+
+export default Carasol
---
+++ components/Header.jsx
@@ -0,0 +1,211 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import Carasol from './Carasol';
+
+const Header=()=>{
+    return (
+        <div className="main">
+            {/* start------ */}
+            <section>
+                <div className="header">
+                  
+
+                    <div className="img" style={{marginLeft:'20px', position: 'relative'}}>
+                        <Image
+                            alt='Mountains'
+                            src='/image/Logo.png'
+                            width={50}
+                            height={30}
+                        />
+                        </div>
+
+                    <div className="text-box">
+                        <Link href="#">Menu</Link>
+                        <Link href="#">About</Link>
+                        <Link href="#">Lab</Link>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className="nav-div">
+                    <div className="box1">
+                    <Image
+                            alt='Mountains'
+                            src='/image/Me.png'
+                            width={200}
+                            height={202}
+                        />
+                    </div>
+                    <div className="box2">
+                        <p>Hello I Am <span>lbrahim Memon</span></p>
+                            <Image className="heay-img"
+                                    alt='Mountains'
+                                    src='/image/Arrow.png'
+                                    width={150}
+                                    height={150}
+                                />
+                        <div>
+                        <p className="banner-title">A Designer Who</p>
+                        <h1 style={{fontFamily: 'Preahvihear'}}>Judges a book by its cover ...</h1>
+                        <div className="box3">
+                    <Image className="secund-img"
+                            alt='Mountains'
+                            src='/image/Ellipse 5.png'
+                            width={160}
+                            height={80}
+                        />
+                    </div>
+                        <small>Because if the cover does not impress you what else can?</small>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className="midil-div">
+                    <div>
+                        <h1 style={{fontFamily: 'Preahvihear'}}>I'm a Software Engineer.|</h1>
+                        <h5>Currently I'm a Software Engineer at Facebook</h5>
+                        <p>Lorem ipsum dolor sit amet 
+                            consectetur adipisicing elit. Quaerat 
+                            provident nam et iste ullam qui enim expedita
+                            reiciendis, consequuntur veritatis. Hic 
+                            praesentium tempora autem illum qui 
+                            voluptates!</p>
+                            </div>
+                    </div>
+            </section>
+
+            <section>
+                <div className="third-div">
+                    <div>
+                    <h1>Work Exprience</h1>
+                    {/* <div className="style-box">
+                        <div className="box4">
+                           <div className="box6" style={{width: '100px', height: '100px', position: 'relative'}}>
+                           <Image
+                            alt='Mountains'
+                            src='/image/Group 2.png'
+                            fill
+                        />
+                           </div>
+                           <div className="text-div">
+                            <h2>CIB on the Mobile</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil tenetur et quae a perspiciatis itaque, facilis eos autem eius distinctio?</p>
+                            <button>LEARN MORE</button>
+                    </div>
+                           </div>
+                           <div className="box4">
+                           <div className="box6" style={{width: '100px', height: '100px', position: 'relative'}}>
+                           <Image
+                            alt='Mountains'
+                            src='/image/Group 1935.png'
+                            fill
+                        />
+                           </div>
+                           <div className="text-div">
+                            <h2>CIB on the Mobile</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil tenetur et quae a perspiciatis itaque, facilis eos autem eius distinctio?</p>
+                            <button>LEARN MORE</button>
+                    </div>
+                           </div>
+                           <div className="box4">
+                           <div className="box6" style={{width: '100px', height: '100px', position: 'relative'}}>
+                           <Image
+                            alt='Mountains'
+                            src='/image/Group 1938.png'
+                            fill
+                        />
+                            </div>
+                           <div className="text-div">
+                            <h2>CIB on the Mobile</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil tenetur et quae a perspiciatis itaque, facilis eos autem eius distinctio?</p>
+                            <button>LEARN MORE</button>
+                    </div>
+                           </div>
+                           <div className="box4">
+                           <div className="box6"  style={{width: '100px', height: '100px', position: 'relative'}}>
+                           <Image
+                            alt='Mountains'
+                            src='/image/icons.png'
+                            fill
+                        />
+                           </div>
+                           <div className="text-div">
+                            <h2>CIB on the Mobile</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil tenetur et quae a perspiciatis itaque, facilis eos autem eius distinctio?</p>
+                            <button>LEARN MORE</button>
+                    </div>
+                           </div>
+                        </div> */}
+                    </div>
+                </div>
+            </section>
+
+            <section id="project-portfolio">
+                <Carasol />
+            </section>
+
+            <section>
+                <div className="fifth-div">
+                    <div className="box-div">
+                        <small>Featured Project</small>
+                        <h2>Example Project</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing
+                             elit. Quaerat, nisi nihil, magni neque,
+                              nulla assumenda sunt sapiente harum
+                               molestias quia aut quis perferendis 
+                               natus mollitia! Vitae vel minima fugit
+                                beatae quas. Perspiciatis quibusdam
+                                 ipsam, corporis unde tempore.</p>
+                    </div>
+                    <div className="small-img" style={{width: '400px', height: '270px', position: 'relative'}}>
+                    <Image
+                            alt='Mountains'
+                            src='/image/portfolio.png'
+                            fill
+                        />
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className="hiper-div">
+                <div className="div-imgs" style={{width: '400px', height: '270px', position: 'relative'}}>
+                    <Image
+                            alt='Mountains'
+                            src='/image/Portfolio.png'
+                            fill
+                        />
+                </div>
+        
+            <div className="box-div box11">
+                        <small>Featured Project</small>
+                        <h2>Example Project</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing
+                             elit. Quaerat, nisi nihil, magni neque,
+                              nulla assumenda sunt sapiente harum
+                               molestias quia aut quis perferendis 
+                               natus mollitia! Vitae vel minima fugit
+                                beatae quas. Perspiciatis quibusdam
+                                 ipsam, corporis unde tempore.</p>
+                </div>
+                </div>
+            </section>
+
+            <section className="footer-div">
+                <div>
+                <h2>Contact</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, harum! Quisquam eius officiis placeat, hic possimus, iure magni et nulla unde consequuntur, consequatur perferendis?</p>
+                <span>ibrhabmemn930@gmail.com</span>
+                </div>
+            </section>
+
+            {/* end -------- */}
+        </div>
+    )
+}
+
+export default Header;
